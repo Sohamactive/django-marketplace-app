0b8aa8be168ddf13708e7a03dc45aa24d030fcec
sohamactive <sharmasoham1427@gmail.com>
2025-07-09 17:29:15 +0530

    allowed-host-added-real-commit

--- project25/settings.py
+++ project25/settings.py
@@ -30,7 +30,7 @@ DEBUG = os.getenv("DEBUG","False")=="True"
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
 
-ALLOWED_HOSTS = ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost").split(",")
+ALLOWED_HOSTS =['*']
 
 
 # Application definition
